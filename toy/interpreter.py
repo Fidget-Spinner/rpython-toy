0bf5a09b7e5e83049a410bbf5d5a9bc4274ffd3f
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2025-10-02 13:19:50 +0100

    fix bugs

--- toy/interpreter.py
+++ toy/interpreter.py
@@ -9,7 +9,7 @@ sys.path.insert(0, "/home/ken/Documents/GitHub/pypy")
 
 from rpython.rlib.jit import JitDriver
 
-jitdriver = JitDriver(greens=['pc'], reds=['stack', 'namespace']) 
+jitdriver = JitDriver(greens=['pc'], reds=['self', 'stack', 'namespace']) 
 class Interpreter:
     def __init__(self, stream):
         self.insn_stream = stream
@@ -17,25 +17,25 @@ class Interpreter:
     def run(self):
         pc = 0
         stack = []
-        namespace = []
+        namespace = {}
         while True:
-            jitdriver.jit_merge_point(pc=pc, stack=stack, namespace=namespace)           
+            jitdriver.jit_merge_point(self=self, pc=pc, stack=stack, namespace=namespace)           
             if pc >= len(self.insn_stream):
                 break
             insn = self.insn_stream[pc]
             opcode = insn[0]
             oparg = insn[1]
             if opcode == Opcode.LOAD_CONST:
-                stack.append(oparg)
+                stack.append(int(oparg))
             elif opcode == Opcode.LOAD_NAME:
                 stack.append(namespace[oparg])
             elif opcode == Opcode.BINARY_OP:
                 lhs = stack[-2]
                 rhs = stack[-1]
-                op = chr(oparg)
-                if op == '<':
+                op = oparg
+                if op == u'<':
                     num = 1 if lhs < rhs else 0
-                elif op == '+':
+                elif op == u'+':
                     num = lhs + rhs
                 else:
                     raise NotImplementedError("Unknown binop")
@@ -50,16 +50,15 @@ class Interpreter:
             elif opcode == Opcode.POP_JUMP_IF_FALSE:
                 flag = stack.pop()
                 if flag == 0:
-                    pc += oparg
+                    pc += int(oparg)
             elif opcode == Opcode.PUSH_NULL:
                 stack.append(0)
             elif opcode == Opcode.JUMP_BACKWARD:
-                pc -= oparg
+                pc -= int(oparg)
             else:
                 raise NotImplementedError("Unknown opcode ")
             pc += 1
-            self.pc = pc
-        # print(self.namespace)
+        # print(namespace)
 
 def jitpolicy(driver):
     from rpython.jit.codewriter.policy import JitPolicy
@@ -69,10 +68,15 @@ import os
 def run(argv):
     fp = os.open(argv[1], os.O_RDONLY, 0777)
     res = []
-    buf = os.read(fp, 4096)
-    for line in buf:
-        opcode, oparg = line.rstrip().split(",")
-        res.append((opcode.decode("utf-8"), int(oparg.decode("utf-8"))))
+    MAX_BUF_LEN = 4096
+    buf = os.read(fp, MAX_BUF_LEN)
+    prev = 0
+    for i in range(len(buf)):
+        if buf[i] == "\n":
+            line = buf[prev:i]
+            prev = i + 1
+            opcode, oparg, _ = line.split(",")
+            res.append((opcode.decode("utf-8"), oparg.decode("utf-8")))
     Interpreter(res).run()
     os.close(fp)
     return 0
@@ -81,8 +85,5 @@ def target(*args):
     return run, None
 
 if __name__ == "__main__":
-   
-    with open("toy.json") as fp:
-        stream = json.load(fp)
-        Interpreter(stream).run()
+   run(sys.argv)
 
