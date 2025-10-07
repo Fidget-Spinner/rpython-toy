5f65b9ae127d4fe73f0966d0cb6afb0de7f5e0cc
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2025-10-07 16:42:23 +0100

    optimize store name and load name

--- toy/interpreter.py
+++ toy/interpreter.py
@@ -7,13 +7,13 @@ import sys
 sys.path.insert(0, "/home/ken/Documents/GitHub/pypy")
 
 
-from rpython.rlib.jit import JitDriver, hint, elidable
+from rpython.rlib.jit import JitDriver, hint, elidable, elidable_promote
 
 def get_location(pc, insn_stream):
     return "%d_%s_%s" % (
             pc, insn_stream[pc][0].encode("ascii"), insn_stream[pc][1].encode("ascii")
     )
-jitdriver = JitDriver(greens=['pc', 'insn_stream'], reds=['stack', 'namespace'], get_printable_location=get_location) 
+jitdriver = JitDriver(greens=['pc', 'insn_stream'], reds=['stack', 'ivar_map', 'localsplus'], get_printable_location=get_location) 
 class Interpreter:
     def __init__(self):
         pass
@@ -24,12 +24,22 @@ class Interpreter:
         return insn_stream[pc]
 
     @staticmethod
+    @elidable
+    def get_ivar(ivar_map, var):
+        return ivar_map[var]
+
+    @staticmethod
+    def get_var(var_table, i):
+        return var_table[i]    
+
+    @staticmethod
     def run(insn_stream):
         pc = 0
         stack = []
-        namespace = {}
+        ivar_map = {} # maps from var name to offset in namespace
+        localsplus = [] # maps from offset to var value
         while True:
-            jitdriver.jit_merge_point(pc=pc, insn_stream=insn_stream, stack=stack, namespace=namespace)           
+            jitdriver.jit_merge_point(pc=pc, insn_stream=insn_stream, stack=stack, ivar_map=ivar_map, localsplus=localsplus)
             if pc >= len(insn_stream):
                 break
             insn = Interpreter.get_insn(insn_stream, pc)
@@ -40,7 +50,12 @@ class Interpreter:
                 oparg = hint(oparg, promote=True)
                 stack.append(int(oparg))
             elif opcode == Opcode.LOAD_NAME:
-                stack.append(namespace[oparg])
+                ivar_map = hint(ivar_map, promote=True)
+                oparg = hint(oparg, promote_unicode=True)
+                offset = Interpreter.get_ivar(ivar_map, oparg)
+                val = Interpreter.get_var(localsplus, offset)
+                # print(oparg, offset, val)
+                stack.append(val)
             elif opcode == Opcode.BINARY_OP:
                 rhs = stack.pop()
                 lhs = stack.pop()
@@ -57,8 +72,11 @@ class Interpreter:
                     raise NotImplementedError(u"Unknown binop %s" % op)
                 stack.append(num)
             elif opcode == Opcode.STORE_NAME:
-                namespace[oparg] = stack[-1]
-                stack.pop()
+                if oparg not in ivar_map:
+                    new_offset = len(ivar_map)
+                    ivar_map[oparg] = new_offset
+                    localsplus.append(-1)
+                localsplus[ivar_map[oparg]] = stack.pop()
             elif opcode == Opcode.DUP:
                 stack.append(stack[-1])
             elif opcode == Opcode.POP_TOP:
@@ -77,7 +95,7 @@ class Interpreter:
                 raise NotImplementedError("Unknown opcode ")
             pc += 1
             if opcode == Opcode.JUMP_BACKWARD:
-                jitdriver.can_enter_jit(pc=pc, insn_stream=insn_stream, stack=stack, namespace=namespace)
+                jitdriver.can_enter_jit(pc=pc, insn_stream=insn_stream, stack=stack, ivar_map=ivar_map, localsplus=localsplus)
         # print(namespace)
 
 def jitpolicy(driver):
